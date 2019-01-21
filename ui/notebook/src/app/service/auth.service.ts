506a3a87cf9a11ffa7339f3e5c9b44cff1b30739
xanderflood <xander.t.flood@gmail.com>
2019-01-20 21:34:12 -0500

    outline of auth service

--- ui/notebook/src/app/service/auth.service.ts
+++ ui/notebook/src/app/service/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { options } from '../../environments/environment';
+
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   //TODO: https://auth0.com/blog/developing-golang-and-angular-apps-part-1-backend-api/
+  tokenVal: string
 
-  constructor() { }
+  constructor() {
+    if (options.token) {
+      this.tokenVal = options.token;
+    }
+  }
 
-  // user uuid: 123e4567-e89b-12d3-a456-426655440000 with signing secret `secret`
-  static DefaultToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjNlNDU2Ny1lODliLTEyZDMtYTQ1Ni00MjY2NTU0NDAwMDAifQ.zfCFqiJ6bKBXKU337T3AZ0uhy4VG-9VpD2GhnhghnQk"
-  get token(): string { return AuthService.DefaultToken; }
+  login(username: string, password: string) { /* TODO */ }
+
+  refresh() { /* TODO */ }
+
+  get token(): string { return this.tokenVal; }
 }
